2b4601b0c8439f833869edab0e6209d863c5fa72
Salavat Nursafin <krowlexing@gmail.com>
2024-12-13 22:59:40 +0400

    add info pages

--- src/components/AppHeader/AppHeader.tsx
+++ src/components/AppHeader/AppHeader.tsx
@@ -1,5 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import { AppMenuItem, HeaderContainer, Text } from "./styles";
+import { useState } from "react";
+import { Paper } from "@mui/material";
 
 interface Props {
     selected: "exercises" | "statistics" | "info" | "exit";
@@ -8,24 +10,78 @@ interface Props {
 export function AppHeader(props: Props) {
     const { selected } = props;
     const nav = useNavigate();
+
+    const [modal, setModal] = useState(false);
+
+    const selection = modal ? "info" : selected;
     return (
         <HeaderContainer>
             <AppMenuItem
-                selected={selected == "exercises"}
+                selected={selection == "exercises"}
                 onClick={() => nav("/exercises")}
             >
                 Упражнения
             </AppMenuItem>
             <AppMenuItem
-                selected={selected == "statistics"}
+                selected={selection == "statistics"}
                 onClick={() => nav("/stats")}
             >
                 Статистика
             </AppMenuItem>
-            <AppMenuItem selected={selected == "info"}>
-                Справочная информация
+            <AppMenuItem
+                selected={selection == "info"}
+                onClick={() => setModal(true)}
+            >
+                <div
+                    style={{
+                        position: "relative",
+                    }}
+                >
+                    <div>Справочная информация</div>
+                    {modal && (
+                        <>
+                            <Paper
+                                sx={{
+                                    zIndex: 10,
+                                    position: "absolute",
+                                    padding: "3px",
+                                    width: "210px",
+                                    top: "50px",
+                                    left: "-5px",
+                                    borderRadius: "5px",
+                                }}
+                                onClick={(e) => e.stopPropagation()}
+                            >
+                                <div style={{ marginBottom: "10px" }}>
+                                    <a href="/dev.html">
+                                        Сведения о разработчиках
+                                    </a>
+                                </div>
+                                <div>
+                                    <a href="/system.html">
+                                        Сведения о системе
+                                    </a>
+                                </div>
+                            </Paper>
+                        </>
+                    )}
+                </div>
             </AppMenuItem>
             <AppMenuItem selected={selected == "exit"}>Выхода нет</AppMenuItem>
+            {modal && (
+                <div
+                    style={{
+                        position: "absolute",
+                        top: 0,
+                        left: 0,
+                        width: "100vw",
+                        height: "100vh",
+                    }}
+                    onClick={() => {
+                        setModal(false);
+                    }}
+                ></div>
+            )}
         </HeaderContainer>
     );
 }
